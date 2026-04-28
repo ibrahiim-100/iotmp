053bb09be49be9011cd540c512f9432eca91bb9e
Mohammed Ibrahim <158835946+ibrahiim-100@users.noreply.github.com>
2026-04-28 18:46:49 +0530

    removed unused files

--- SQL.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE safety_data (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  temperature FLOAT,
-  smoke VARCHAR(10),
-  fire VARCHAR(10),
-  light_status VARCHAR(10),
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
